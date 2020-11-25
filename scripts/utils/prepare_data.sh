ad4214e9f12f40182855d593d28ad88562b94c54
Dat Duong <datd1988@gmail.com>
2020-11-25 16:36:46 -0500

    add our script

--- scripts/utils/prepare_data.sh
+++ scripts/utils/prepare_data.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 python make_hdf5.py --dataset I128 --batch_size 256 --data_root data
 python calculate_inception_moments.py --dataset I128_hdf5 --data_root data
+
+
+# ! my own stuffs. 
+cd /data/duongdb/BigGAN-PyTorch/
+python make_hdf5.py --dataset I128 --batch_size 256 --data_root /data/duongdb/SkinConditionImages11052020/Recrop/OneLabelOneFolder/
